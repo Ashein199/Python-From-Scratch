3532b12d61f580f848993d25f9ba552952fbeccc
Ashein199 <heroglasses155@gmail.com>
2021-10-23 21:36:17 +0630

    input-box & type casting

---
+++ input-box.py
@@ -0,0 +1,15 @@
+# name = input("Name : ");
+# print('hello '+name)
+
+# age = input("Tell ur age : ");
+# print('Your age is '+age)
+
+# area calculator(pi r**2) 
+
+PI = 3.14;
+
+r = int(input('radius : '))
+
+area=PI*r**2;
+
+print(area);
