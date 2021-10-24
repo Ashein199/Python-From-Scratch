bb8ef30b5767db61bac9c01b1cd0433700283d4c
Ashein199 <heroglasses155@gmail.com>
2021-10-24 17:38:34 +0630

    for loop

---
+++ loops.py
@@ -0,0 +1,12 @@
+# names =["Kyaw Kyaw","John","Nine","Mg Mg"];
+
+# for name in names :
+#     if(name == "John"):
+#         print(f'{name} is a foreigner.');
+#     else :
+#         print(f'{name} is Myanmar.')
+
+fruits =["Apple", "Orange", "Banana"];
+
+for fruit in fruits :
+    print(f'{fruit} is a fruit .');
