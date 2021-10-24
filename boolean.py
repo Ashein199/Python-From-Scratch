d594ffc93d5223142db30801cffd9c5e9b85492e
Ashein199 <heroglasses155@gmail.com>
2021-10-24 17:38:07 +0630

    booleans and operators

---
+++ boolean.py
@@ -0,0 +1,21 @@
+# hungry = True
+# print(hungry);
+
+# Comparison operators : < > <= >= ==
+
+# print(5>3);
+# print(9<1);
+
+# print(5>=5);
+
+# print(3<=5);
+
+# print(3==1);
+
+# Logical operators :: and or not
+
+# print(5==5 and 4<1);
+
+# print(not True);
+
+# print(5<3 or 5>=2);
