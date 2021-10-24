9c3dfff3442f150032f0300df68c2a8d00b16fa5
Ashein199 <heroglasses155@gmail.com>
2021-10-24 15:56:38 +0630

    if_else conditions

---
+++ if_else.py
@@ -0,0 +1,28 @@
+# age = int(input('Your age: '));
+
+# if age < 18 :
+#     print("Just a kid Haha...");
+# elif age > 18 and age <30 :
+#     print("Wow .. Adult");
+# else :
+#     print("Be ready to die");
+
+    # print("hi");
+
+# tired=input("Are u tired? y/n: "); #yes or no
+
+# if tired=="y" :
+#     print("take a rest");
+# elif tired == "n":
+#     print("go back to work");
+# else :
+#     print("plz enter y or n")
+
+username = input('username: ');
+password = input('password: ');
+
+if username == "amm" and password == "123":
+    print("Login successfully");
+
+else :
+    print('Check ur name & password');
