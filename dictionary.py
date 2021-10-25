7bf93747bb4c3384d57f27e0d5a322d63e9b0ea1
Ashein199 <heroglasses155@gmail.com>
2021-10-25 20:04:16 +0630

    dictionaries

---
+++ dictionary.py
@@ -0,0 +1,28 @@
+person={
+    "name": "Entt Nine",
+    "age" : "23"
+}
+
+# person['hair_color']="black";
+# print(person);
+
+# print("age" in person);
+# if "name" in person : #true
+#     print(person)
+
+# if "nth" in person : #false
+#     print("work");
+
+characteristics = list(person.keys());
+print(characteristics);
+
+details = list(person.values());
+print(details);
+
+# list={}
+
+# key = input("Enter key :");
+# value = input("Enter value :");
+
+# list[key]=value;
+# print(list)
