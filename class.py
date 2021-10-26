a5d088aef15358e5f84f264516ecd3ba6dffeec0
Ashein199 <heroglasses155@gmail.com>
2021-10-26 19:22:46 +0630

    level of attributes and methods

--- class.py
+++ class.py
@@ -1,20 +1,28 @@
 class Car:
-    def __init__(self,name,license,color):
+    steeringWheel = 1  #class level object
+    def __init__(self,name,license,color):#instance object
         self.name = name
         self.license =license
         self.color = color
     def drive(self):
         print(f'{self.name} is driving');
-    def isLicensed(self):
+    def isLicensed(self): #instance level method
         if self.license==True:
             print("It has license");
         else :
             print("Without!");
+    @classmethod
+    def common(cls): #class level
+        print(f'All cars have only {cls.steeringWheel} steering wheel');
 
-lambo = Car("Lamborghini", True,"Red");
-lambo.drive();
-lambo.isLicensed();
+# lambo = Car("Lamborghini", True,"Red");
+# lambo.drive();
+# lambo.isLicensed();
 
-Marcedes = Car("Marcedes",False,"Black");
-Marcedes.drive();
-Marcedes.isLicensed();
+# Marcedes = Car("Marcedes",False,"Black");
+# Marcedes.drive();
+# Marcedes.isLicensed();
+
+print(Car.steeringWheel);
+Mark = Car("Mark",True,"White");
+Mark.common();
