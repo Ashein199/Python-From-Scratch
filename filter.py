4f9460a1d9852d3b7352970664bf64c3c042a835
Ashein199 <heroglasses155@gmail.com>
2021-10-28 20:41:44 +0630

    lambda functions

--- filter.py
+++ filter.py
@@ -1,10 +1,14 @@
 nums = [1,2,3,4,5,6,7,8,9,10]
 
-def even(num): #filter way
-    return (num%2)==0
+# def even(num): #filter way
+#     return (num%2)==0
+# even_nums = list(filter(even,nums))
+# print(even_nums)
 
-even_nums = list(filter(even,nums))
+# ____ lambda____
+even_nums =list(filter(lambda num:(num%2)==0,nums))
 print(even_nums)
+
 
 # nums = [num for num in nums if(num%2)==0] #comprehension way
 # print(nums)
--- map.py
+++ map.py
@@ -1,10 +1,14 @@
 nums =[2,3,5,7,8,12]
 
-def mapFunction(num):2  #map
-    return num**2
-
-nums = list(map(mapFunction,nums));
-print(nums)
+# def mapFunction(num):2  #map
+#     return num**2
+# nums = list(map(mapFunction,nums));
+# print(nums)
 
 # nums=[num**2 for num in nums] #comprehension way
 # print(nums)
+
+# ___lambda___
+
+nums = list(map(lambda num:num**2,nums))
+print(nums)
