a333f47ebc35b11a4a394047baa44d3341b39c32
Ashein199 <heroglasses155@gmail.com>
2021-10-28 20:25:39 +0630

    filter function of list

---
+++ filter.py
@@ -0,0 +1,16 @@
+nums = [1,2,3,4,5,6,7,8,9,10]
+
+def even(num): #filter way
+    return (num%2)==0
+
+even_nums = list(filter(even,nums))
+print(even_nums)
+
+# nums = [num for num in nums if(num%2)==0] #comprehension way
+# print(nums)
+
+# even_nums = [] #normal way
+# for num in nums: 
+#     if(num%2)==0:
+#         even_nums.append(num)
+# print(even_nums)
